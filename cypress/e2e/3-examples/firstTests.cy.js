e1f433e1e2cdfd1ad57c9c6935d89fb0c3ebcba9
HAEFELESOFTWARE\stephen <stephen@haefelesoftware.com>
2023-06-22 14:41:29 +0200

    trying to force a conflict

--- cypress/e2e/3-examples/firstTests.cy.js
+++ cypress/e2e/3-examples/firstTests.cy.js
@@ -18,6 +18,7 @@ describe(`A collection of first cypress tests`, () => {
     afterEach(`Remove items from cart`, () => {
         cy.visit(`/cart`);      
         cy.visit(`/cart`);
+        cy.visit(`conflict please`);
        cy.clearCart();
     })
 
